5a196d9119176c6fc20f17f1383e9bef628602aa
Abat <personal@abat.me>
2023-05-13 12:32:18 +0500

    feat: Add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '~/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Head from 'next/head'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Bizler Group</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
