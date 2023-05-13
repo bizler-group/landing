c8573fd9f379a293db98495c43c804a121fe6114
Abat <personal@abat.me>
2023-05-13 12:37:33 +0500

    chore: Replace favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Bizler Group</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Bizler" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Component {...pageProps} />
