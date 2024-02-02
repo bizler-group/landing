0b3d7dae6a329e1ccf81687a8a8d4e0060a78548
IlalovX <ilalovx@gamil.com>
2024-02-02 14:17:36 +0500

    add content

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,35 +1,36 @@
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
-import { IconArrowUpRight } from '@tabler/icons-react'
-import useTranslation from 'next-translate/useTranslation'
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { IconArrowUpRight } from "@tabler/icons-react";
+import useTranslation from "next-translate/useTranslation";
 
-import { Button } from '~/components/ui/Button'
-import { ProjectCard } from '~/components/ui/ProjectCard'
-import { PROJECT_SLIDES } from '~/constants/data'
-import { nunitoSans } from '..'
-import { useRouter } from 'next/router'
+import { Button } from "~/components/ui/Button";
+import { ProjectCard } from "~/components/ui/ProjectCard";
+import { PROJECT_SLIDES } from "~/constants/data";
+import { nunitoSans } from "..";
+import { useRouter } from "next/router";
 
 const PROJECT_TYPES = [
-  'all',
-  'website',
-  'crm',
+  "all",
+  "website",
+  "crm",
+  "design",
   // 'mobile_app',
-] as const
+] as const;
 
 export default function Project() {
-  const { t } = useTranslation('projects')
-  const router = useRouter()
-  const [hoveredImageIndex, setHoveredImageIndex] = useState(-1)
+  const { t } = useTranslation("projects");
+  const router = useRouter();
+  const [hoveredImageIndex, setHoveredImageIndex] = useState(-1);
   const [projectType, setProjectType] =
-    useState<(typeof PROJECT_TYPES)[number]>('all')
+    useState<(typeof PROJECT_TYPES)[number]>("all");
 
   useEffect(() => {
-    const { type } = router.query
+    const { type } = router.query;
 
-    if (typeof type === 'string' && PROJECT_TYPES.includes(type as any)) {
-      setProjectType(type as any)
+    if (typeof type === "string" && PROJECT_TYPES.includes(type as any)) {
+      setProjectType(type as any);
     }
-  }, [router])
+  }, [router]);
 
   return (
     <main
@@ -37,7 +38,7 @@ export default function Project() {
     >
       <div>
         <h1 className="text-4xl font-bold text-white mb-7 mt-20">
-          {t('portfolio')}
+          {t("portfolio")}
         </h1>
 
         <div className="flex flex-wrap gap-3">
@@ -49,7 +50,7 @@ export default function Project() {
               active={projectType === type}
               onClick={() => setProjectType(type)}
             >
-              {t(type === 'all' ? 'common:all_projects' : t(type))}
+              {t(type === "all" ? "common:all_projects" : t(type))}
             </Button>
           ))}
         </div>
@@ -57,7 +58,7 @@ export default function Project() {
 
       <div className="mt-8 mb-20 grid grid-cols-4 gap-y-7 gap-x-5 max-md:grid-cols-1">
         {PROJECT_SLIDES.filter(
-          (project) => projectType === 'all' || project.type === projectType
+          (project) => projectType === "all" || project.type === projectType
         ).map((project, index) => (
           <ProjectCard
             key={project.id}
@@ -73,7 +74,7 @@ export default function Project() {
                 href={project.link}
                 className="flex text-white absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
               >
-                {t('common:open_details')}{' '}
+                {t("common:open_details")}{" "}
                 <IconArrowUpRight className="text-[#52B6C4]" />
               </Link>
             )}
@@ -81,5 +82,5 @@ export default function Project() {
         ))}
       </div>
     </main>
-  )
+  );
 }
